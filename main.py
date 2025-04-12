1474e972eb0316453754e4ea3723607fa3f84957
Андрей Самородов <andrey.samorodow@gmail.com>
2025-04-12 20:46:08 +0700

    Переделал интерфейс с помощью flet

--- main.py
+++ main.py
@@ -1,46 +1,57 @@
+import flet as ft
 from g4f.client import Client
-import tkinter as tk
-from tkinter import *
-from tkinter import messagebox
-import time
 
-window = Tk() #Создаём окно приложения.
-window.title("ALOE")
-window.geometry('550x400')
+def main(page: ft.Page):
+    page.title = "ALOE"
+    page.window.icon = "Materials/icon.png"
+    page.theme_mode = "dark"
+    page.window.width = 600
+    page.window.height = 1000
+    page.window.min_width = 600
+    page.window.min_height = 500
+    page.window.always_on_top = True
 
-def insert(text):
-    words = text.split()  # Разделяем строку на слова
-    result = []  # Список для результата
+    def insert(text):
+        words = text.split()  # Разделяем строку на слова
+        result = []  # Список для результата
+        for i in range(0, len(words), 12):
+            result.append(' '.join(words[i:i+12]))  # Объединяем каждые 6 слов
+        return '\n'.join(result)  # Объединяем результаты с переводами строки
 
-    for i in range(0, len(words), 6):
-        result.append(' '.join(words[i:i+6]))  # Объединяем каждые 6 слов
-
-    return '\n'.join(result)  # Объединяем результаты с переводами строки
- 
-
-
-lbl = Label(window, text="Введите запрос вручную\n или оставьте поле пустым для скриншота", font=("Arial Bold", 20))  
-lbl.grid(column=0, row=1)
-
-txt = Entry(window,width=80,)  
-txt.grid(column=0, row=0)
-
-answer = Label(window, text='')
-answer.grid(column=0, row=3)
-
-def clicked():
-    from Apps.answer import defining_task
-    def search_answer(text=defining_task()):
+    def search_answer(text="defining_task()"):
         client = Client()
-        response = client.chat.completions.create(model="gpt-4o", messages=[{"role": "user", "content": f"{text}. Дай только четкий ответ, либо ответ из предложенных вариантов, если не знаешь пиши незнаю. После ответа писать ничего не нужно. и переноси строки"}],)
-        return (response.choices[0].message.content)
-    if txt.get() == '':
-        answer.configure(text=insert(search_answer()))
-    else:
-        answer.configure(text=insert(search_answer(txt.get())))
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[{"role": "user", "content": f"{text}. Дай только четкий ответ, либо ответ из предложенных вариантов, если не знаешь пиши незнаю. После ответа писать ничего не нужно."}],
+        )
+        return response.choices[0].message.content
 
-btn = Button(window, text="Начать поиск", bg="green", fg="white", command=clicked)
-btn.grid(column=0, row=2) 
+    def event(e):
+        from Apps.answer import defining_task
+        answer_text = insert(search_answer(defining_task()))
+        text_widget.value = answer_text
+        page.update()
 
+    text_widget = ft.Text("", text_align=ft.TextAlign.CENTER)
 
-window.mainloop()
+    page.add(
+        ft.Row([ft.Image(src="Materials/img.png")], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([ft.Image(src="Materials/img2.png")], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row(
+            [
+                ft.ElevatedButton(
+                    adaptive=True,
+                    content=ft.Text("НАЧАТЬ", color=ft.Colors.WHITE),
+                    bgcolor=ft.CupertinoColors.ACTIVE_GREEN,
+                    on_click=event 
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        ),
+        ft.Row(
+            [text_widget],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
+
+ft.app(target=main)
